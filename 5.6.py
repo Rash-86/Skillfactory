aa8574cd6cd71c7ef19de91fcc9125ae0f065ad9
Rash-86 <97832965+Rash-86@users.noreply.github.com>
2022-02-17 17:16:39 +0500

    Add files via upload

---
+++ 5.6.py
@@ -0,0 +1,95 @@
+def greet():
+    print("-------------------")
+    print("  Приветсвуем вас  ")
+    print("      в игре       ")
+    print("  крестики-нолики  ")
+    print("-------------------")
+    print(" формат ввода: x y ")
+    print(" x - номер строки  ")
+    print(" y - номер столбца ")
+
+
+def show():
+    print("     | 0 | 1 | 2 | ")
+    print("  --------------- ")
+    for i in field:
+        row_str = f" | {i} | {' | '.join(field[i])} | "
+        print(row_str)
+        print("  --------------- ")
+    print()
+
+
+def ask():
+    count = 0
+    while True:
+        if count == 0:
+            cords = input("         Ходи: ").split()
+            count += 1
+        else:
+            cords = input("         Поробуй еще раз: ").split()
+
+        if len(cords) != 2:
+            print(" Введите 2 координаты! ")
+            continue
+
+        x, y = cords
+
+        if not (x.isdigit()) or not (y.isdigit()):
+            print(" Введите числа! ")
+            continue
+
+        x, y = int(x), int(y)
+
+        if 0 > x or x > 2 or 0 > y or y > 2:
+            print(" Координаты вне диапазона! ")
+            continue
+
+        if field[x][y] != " ":
+            print(" Клетка занята! ")
+            continue
+
+        return x, y
+
+
+def check_win():
+    win_cord = (((0, 0), (0, 1), (0, 2)), ((1, 0), (1, 1), (1, 2)), ((2, 0), (2, 1), (2, 2)),
+                ((0, 2), (1, 1), (2, 0)), ((0, 0), (1, 1), (2, 2)), ((0, 0), (1, 0), (2, 0)),
+                ((0, 1), (1, 1), (2, 1)), ((0, 2), (1, 2), (2, 2)))
+    for cord in win_cord:
+        symbols = []
+        for c in cord:
+            symbols.append(field[c[0]][c[1]])
+        if symbols == ["X", "X", "X"]:
+            print("Выиграл X!!!")
+            return True
+        if symbols == ["0", "0", "0"]:
+            print("Выиграл 0!!!")
+            return True
+    return False
+
+
+greet()
+field = {0: [" "] * 3, 1: [" "] * 3, 2: [" "] * 3}
+count = 0
+
+while True:
+    count += 1
+    show()
+    if count % 2 == 1:
+        print(" Ходит крестик!")
+    else:
+        print(" Ходит нолик!")
+
+    x, y = ask()
+
+    if count % 2 == 1:
+        field[x][y] = "X"
+    else:
+        field[x][y] = "0"
+
+    if check_win():
+        break
+
+    if count == 9:
+        print(" Ничья!")
+        break
